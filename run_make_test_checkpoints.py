41a0478e64f28241e9127da24b19b71a808e5014
Evan Racah <ejracah@gmail.com>
2024-01-29 15:56:34 -0800

    fixes to update repo name

--- run_make_test_checkpoints.py
+++ run_make_test_checkpoints.py
@@ -7,17 +7,17 @@ from mcli.sdk import RunConfig, create_run, wait_for_run_status
 gpu_num=8 # 1
 cluster='r1z1'
 images = ['mosaicml/pytorch:2.0.1_cu118-python3.10-ubuntu20.04'] #, 'mosaicml/pytorch:1.13.1_cu117-python3.10-ubuntu20.04']
-composer_versions = ['v0.15.1'] # ['v0.13.5', 'v0.14.0', 'v0.14.1', 'v0.15.0',  'v0.16.0']
+composer_versions = ['v0.18.1'] # ['v0.13.5', 'v0.14.0', 'v0.14.1', 'v0.15.0',  'v0.16.0']
 
 
 manual_test_integration = {
     'integration_type': 'git_repo',
-    'git_repo': 'eracah/composer-manual-tests',
+    'git_repo': 'eracah/composer-test-ckpts',
     'git_branch': 'master',
-    'path': '/tmp/composer-manual-tests'
+    'path': '/tmp/composer-test-ckpts'
 }
 
-fsdp_state_dict_types = ['sharded', 'full', 'local']
+fsdp_state_dict_types = ['sharded', 'full']
 precisions = ['amp_fp16', 'amp_bf16']
 sharding_strategies = ['SHARD_GRAD_OP', 'FULL_SHARD']
 
@@ -34,7 +34,7 @@ for fsdp_state_dict_type, precision, sharding_strategy, image, composer_version 
 
     integrations = [manual_test_integration, composer_integration]
     pt_version = image.split(':')[1].split('_')[0]
-    cmd = f'pip install pydantic==1.10.12; cd /tmp/composer-manual-tests/test-ckpt && composer -n 2 make_test_ckpt.py {fsdp_state_dict_type} {precision} {sharding_strategy}'
+    cmd = f'pip install pydantic==1.10.12; cd /tmp/composer-test-ckpts && composer -n 2 make_test_ckpt.py {fsdp_state_dict_type} {precision} {sharding_strategy}'
    
     run_name = f"bcompat-{fsdp_state_dict_type}-{precision.split('_')[-1]}-pt-{pt_version.replace('.', '-')}-cp-{composer_version.replace('.', '-')}"
     print(run_name)
